e57edb212226b38adbe0da4870deb5bafd72aba0
mohitgupta1918 <mohitgupta1918@gmail.com>
2015-02-28 02:43:22 +0530

    Initial commit

---
+++ mobile/src/androidTest/java/com/github/mohitgupta1918/r0/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.github.mohitgupta1918.r0;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
